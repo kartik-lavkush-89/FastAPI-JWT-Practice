92b504ca57e7d749613b1d515a292963d5324dd9
Kartik Lavkush <kartik.lavkush@unthinkable.co>
2023-04-19 19:29:07 +0530

    modify html code

--- src/routes/views.py
+++ src/routes/views.py
@@ -9,6 +9,7 @@ from pymongo import MongoClient
 import os
 import bcrypt
 import jwt
+
 # import datetime
 from datetime import datetime, timedelta
 import time
@@ -31,15 +32,13 @@ conn = MongoClient()
 redis_cache = redis.Redis(host="localhost", port=6379, db=0)
 
 
-
 templates = Jinja2Templates(directory="templates")
 
 
-#signup route
-#adding user details to the database
+# signup route
+# adding user details to the database
 @api.post("/signup")
 async def signup(user: Signup):
-
     # Check if the phone number already exists
     phone_number = conn.tokenPractice.data.find_one({"phone": user.phone})
     if not phone_number:
@@ -69,12 +68,10 @@ async def signup(user: Signup):
         raise HTTPException(status_code=404, detail="phone_already_exist!")
 
 
-
-#login route
-#verifying user through credentials
+# login route
+# verifying user through credentials
 @api.post("/login")
 async def login(details: Login):
-
     # Check if the email exists
     email_id = conn.tokenPractice.data.find_one({"email": details.email})
     if email_id:
@@ -85,7 +82,7 @@ async def login(details: Login):
             # Create a new JWT token
             payload = {
                 "email": details.email,
-                "iat" : datetime.utcnow(),
+                "iat": datetime.utcnow(),
                 "exp": datetime.utcnow() + timedelta(minutes=30),
             }
             token = jwt.encode(payload, secret_key, "HS256")
@@ -97,15 +94,13 @@ async def login(details: Login):
     raise HTTPException(status_code=404, detail="email_doesn't_exist!")
 
 
-
 # protected route
-#retrieving data from database by verifying user through token
+# retrieving data from database by verifying user through token
 @api.get("/get_data")
 async def get_data(token: str = Header(...)):
-
     # Check if the token is revoked
-    if redis_cache.sismember("revoked",token):
-         return {"message": "Unauthorized"}
+    if redis_cache.sismember("revoked", token):
+        return {"message": "Unauthorized"}
     else:
         # Verify the JWT token and get the user's email
         user_email = token_required(token)
@@ -115,56 +110,66 @@ async def get_data(token: str = Header(...)):
         return userEntity(user_data)
 
 
-
-
 @api.post("/blacklist/{token}")
 async def blacklist(token: str):
     # Add the token to the Redis cache
-    redis_cache.sadd("revoked",token)
-    
+    redis_cache.sadd("revoked", token)
+
     return {"message": "Token is Blacklisted"}
 
 
-
-
-@api.get("/revoked-tokens",response_class=HTMLResponse)
-async def get_blacklisted_tokens(request : Request):
+@api.get("/revoked-tokens", response_class=HTMLResponse)
+async def get_blacklisted_tokens(request: Request):
     # Retrieve the list of blacklisted tokens from Redis cache
     revoked_tokens = list(redis_cache.smembers("revoked"))
-    
-    return templates.TemplateResponse("revoked_tokens.html", {"request": request, "revoked_tokens": revoked_tokens})
+
+    return templates.TemplateResponse(
+        "revoked_tokens.html", 
+        {
+            "request": request,
+            "revoked_tokens": revoked_tokens
+        }
+    )
 
 
-
-@api.post("/whitelist/{token}")
+@api.post("/whitelist/b'{token}'")
 async def whitelist(token: str):
     # Delete the token from the Redis cache
-    redis_cache.srem("revoked", token)
-    
+    redis_cache.srem("revoked",token)
+
     return {"message": "Token is Whitelisted"}
 
 
 @api.get("/info/{token}", response_class=HTMLResponse)
-async def token_info(request:Request, token: str):
-   
+async def token_info(request: Request, token: str):
+
     token_required(token)
 
-    if redis_cache.sismember("revoked",token):
-         return {"message": "Unauthorized"}
+    if redis_cache.sismember("revoked", token):
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
-    else :
-    # Decode the token to get the payload
-        try:
-            payload = jwt.decode(token, secret_key, algorithms=["HS256"])
-        except jwt.InvalidTokenError:
-            return {"error": "Invalid token"}
+    
+        # Decode the token to get the payload
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+    except jwt.InvalidTokenError:
+        return {"error": "Invalid token"}
 
-        # Extract the relevant information from the payload
-        email = payload.get("email")
-        info = conn.tokenPractice.data.find_one({"email": email})
-        username = info.get("username")
-        issued_at = datetime.fromtimestamp(payload.get("iat"))
-        expires_at = datetime.fromtimestamp(payload.get("exp"))
+    # Extract the relevant information from the payload
+    email = payload.get("email")
+    info = conn.tokenPractice.data.find_one({"email": email})
+    username = info.get("username")
+    issued_at = datetime.fromtimestamp(payload.get("iat"))
+    expires_at = datetime.fromtimestamp(payload.get("exp"))
 
-        
-        return templates.TemplateResponse("index.html",{"request":request, "username" : username,"email" : email, "token" : token, "issued_at" : issued_at, "expires_at" : expires_at})
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "username": username,
+            "email": email,
+            "token": token,
+            "issued_at": issued_at,
+            "expires_at": expires_at,
+        }
+    )
